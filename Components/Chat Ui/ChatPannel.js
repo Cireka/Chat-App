e60cd794f55a74e4a867a47e3e49fb870e1bcaba
Cireka <tsifekide@gmail.com>
2023-06-20 23:15:58 +0400

    fixed issue with multiple connections

--- Components/Chat Ui/ChatPannel.js
+++ Components/Chat Ui/ChatPannel.js
@@ -1,17 +1,62 @@
+"use client";
+import { useState, useEffect } from "react";
 import style from "./ChatPannel.module.css";
 import { BsFillSendFill } from "react-icons/bs";
 import { MdAttachFile } from "react-icons/md";
 
-export default function ChatPannel() {
+export default function ChatPannel({ socket }) {
+  const [message, setMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
+
+  const handleMessageSubmit = () => {
+    // send message
+    socket.emit("chatMessage", message);
+    // ande after sending it clear input
+    setMessage("");
+  };
+
+  socket.on("message", (msg) => {
+    setChatMessages([...chatMessages, msg]);
+  });
+
+  const SubmitHandller = (event) => {
+    event.preventDefault();
+    if (message !== "") {
+      handleMessageSubmit();
+    }
+  };
+  const enterHandller = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      handleMessageSubmit();
+    }
+  };
+  const typeHandller = (event) => {
+    const message = event.target.value;
+    setMessage(message);
+  };
   return (
-    <div className={style.ChatPannel}>
-      <textarea className={style.chat}></textarea>
-      <div className={style.IconParrent}>
-        <div className={style.sendIconParrent}>
-          <BsFillSendFill className={style.sendIcon} />
+    <form>
+      <div className={style.ChatPannel}>
+        {chatMessages &&
+          chatMessages.map((message) => {
+            return <p>{message}</p>;
+          })}
+        <textarea
+          onKeyDown={enterHandller}
+          onChange={typeHandller}
+          value={message}
+          className={style.chat}
+        ></textarea>
+        <div className={style.IconParrent}>
+          <div className={style.sendIconParrent}>
+            <BsFillSendFill
+              onClick={SubmitHandller}
+              className={style.sendIcon}
+            />
+          </div>
+          <MdAttachFile className={style.AttachIcon} />
         </div>
-        <MdAttachFile className={style.AttachIcon} />
       </div>
-    </div>
+    </form>
   );
 }
--- Components/main/Chat.js
+++ Components/main/Chat.js
@@ -1,16 +1,36 @@
-import { Fragment } from "react";
+"use client";
+import { Fragment, useState, useEffect } from "react";
 import style from "./Chat.module.css";
 import Header from "../Header/Header";
 import ChatWrap from "../ChatWrap/ChatWrap";
 import ContactsPannel from "../Chat Ui/ContactsPannel";
 import ChatPannel from "../Chat Ui/ChatPannel";
+import { io } from "socket.io-client";
+
 export default function Chat() {
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    const socket = io.connect("http://localhost:3001");
+
+    setSocket(socket);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  // All of the code below happends when user connects!
+  // because that how we configured it in the backend.
+  // socket.on("message", (message) => {
+  //   // console.log(message);
+  // });
+
   return (
     <Fragment>
       <Header />
       <ChatWrap>
         <ContactsPannel />
-        <ChatPannel />
+        {socket && <ChatPannel socket={socket} />}
       </ChatWrap>
     </Fragment>
   );
